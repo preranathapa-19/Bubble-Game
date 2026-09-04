cd0f9344b4ea16fa65dd54d822cbb01b7d3d4c2a
preranathapa-19 <preranamagar19@gmail.com>
2026-09-04 23:03:57 +0545

    fix: update bubble generation logic to include random values

--- bubble.js
+++ bubble.js
@@ -1,9 +1,12 @@
+function makebubble(){
 let clutter = "";
 
-for(var i = 0; i<=130; i++){
-   clutter += `<div class = "bubble" >5</div>`;
+for(var i = 0; i<=132; i++){
+   let ran = Math.floor(Math.random()*10)
+   clutter += `<div class = "bubble">${ran}</div>`;
 }
 
+document.querySelector("#pbtm").innerHTML = clutter;
+}
 
-
-   document.querySelector("#pbtm").innerHTML = clutter;
+makebubble()
