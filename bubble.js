3ca5c9945c24667f7f1b9e8e0c0bf01ae015236a
preranathapa-19 <preranamagar19@gmail.com>
2026-09-05 22:22:45 +0545

    fix: implement timer functionality and update timer display in HTML

--- bubble.js
+++ bubble.js
@@ -9,4 +9,16 @@ for(var i = 0; i<=132; i++){
 document.querySelector("#pbtm").innerHTML = clutter;
 }
 
+var timer = 60;
+function setTimer(){
+ let timeint= setInterval(function(){
+   if(timer > 0){
+       timer--;
+      document.querySelector("#timerval").textContent = timer;
+   }else{
+      clearInterval(timeint);
+   } 
+   },1000);
+}
+setTimer()
 makebubble()
